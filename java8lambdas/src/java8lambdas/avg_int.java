c2c38475551529f02a09c54b025f6693264a2514
VADDI SAI PRAVALLIKA <vaddi.saipravallika@gmail.com>
2020-07-24 10:49:27 +0530

    lambdas

---
+++ java8lambdas/src/java8lambdas/avg_int.java
@@ -0,0 +1,21 @@
+package java8lambdas;
+
+import java.util.*; 
+import java.util.stream.*; 
+
+public class avg_int {
+	public static void main(String[] args) 
+    {
+	 	int a[]= {1,4,5,6,7,8,9,45,6};
+        IntStream stream = IntStream.of(a); 
+        OptionalDouble obj = stream.average(); 
+        if (obj.isPresent()) { 
+            System.out.println(obj.getAsDouble()); 
+        } 
+        else { 
+            System.out.println("-1"); 
+        } 
+    } 
+
+
+}
